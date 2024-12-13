f596b0339242e0ad47f70de74087f2ebafed00eb
Soto3619 <nestor.sotoc@istpargentina.edu.pe>
2024-12-13 10:30:36 -0500

    Add files via upload

---
+++ CapaEntidad/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    class ServicioE
+    {
+        public string codigo { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public string precio { get; set; }
+        public string duracion { get; set; }
+    }
+}
